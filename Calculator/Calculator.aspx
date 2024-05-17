a399c675f0a66cc94b771d9b32945dafa801171f
tudorvalentine <tudor1903@gmail.com>
2024-05-17 11:35:41 +0300

    Add project files.

---
+++ Calculator/Calculator.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="WebFormsCalculator.Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculator</title>
+    <link rel="stylesheet" type="text/css" href="Styles.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="calculator">
+            <div class="display">
+                <asp:TextBox ID="txtDisplay" runat="server" CssClass="display-input" ReadOnly="true"></asp:TextBox>
+            </div>
+            <div class="buttons">
+                <asp:Button ID="btnMC" runat="server" Text="MC" CssClass="btn btn-light" OnClick="MemoryClear" />
+                <asp:Button ID="btnMR" runat="server" Text="MR" CssClass="btn btn-light" OnClick="MemoryRecall" />
+                <asp:Button ID="btnMMinus" runat="server" Text="M-" CssClass="btn btn-light" OnClick="MemoryMinus" />
+                <asp:Button ID="btnMPlus" runat="server" Text="M+" CssClass="btn btn-light" OnClick="MemoryPlus" />
+                <asp:Button ID="btnClear" runat="server" Text="C" CssClass="btn btn-red" OnClick="ClearDisplay" />
+                <asp:Button ID="btn7" runat="server" Text="7" CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btn8" runat="server" Text="8" CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btn9" runat="server" Text="9" CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btnDivide" runat="server" Text="÷" CssClass="btn btn-gray" OnClick="Button_Click" />
+                <asp:Button ID="btn4" runat="server" Text="4" CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btn5" runat="server" Text="5" CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btn6" runat="server" Text="6" CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btnMultiply" runat="server" Text="×" CssClass="btn btn-gray" OnClick="Button_Click" />
+                <asp:Button ID="btn1" runat="server" Text="1" CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btn2" runat="server" Text="2" CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btn3" runat="server" Text="3" CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btnMinus" runat="server" Text="-" CssClass="btn btn-gray" OnClick="Button_Click" />
+                <asp:Button ID="btn0" runat="server" Text="0" CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btnDecimal" runat="server" Text="." CssClass="btn btn-light" OnClick="Button_Click" />
+                <asp:Button ID="btnEquals" runat="server" Text="=" CssClass="btn btn-blue" OnClick="Evaluate" />
+                <asp:Button ID="btnPlus" runat="server" Text="+" CssClass="btn btn-gray" OnClick="Button_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Calculator/Calculator.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsCalculator
+{
+    public partial class Default : Page
+    {
+        private static string _input = string.Empty;
+        private static string _operator = string.Empty;
+        private static double _result = 0;
+        private static double _memory = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                string value = button.Text;
+
+                if ("0123456789.".Contains(value))
+                {
+                    if (value == "." && _input.Contains(".")) return;
+                    _input += value;
+                    txtDisplay.Text = _input;
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(_input))
+                    {
+                        _result = double.Parse(_input);
+                        _input = string.Empty;
+                    }
+                    _operator = value;
+                }
+            }
+        }
+
+        protected void ClearDisplay(object sender, EventArgs e)
+        {
+            _input = string.Empty;
+            _operator = string.Empty;
+            _result = 0;
+            txtDisplay.Text = string.Empty;
+        }
+
+        protected void Evaluate(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_input))
+            {
+                double secondNumber = double.Parse(_input);
+                switch (_operator)
+                {
+                    case "+":
+                        _result += secondNumber;
+                        break;
+                    case "-":
+                        _result -= secondNumber;
+                        break;
+                    case "×":
+                        _result *= secondNumber;
+                        break;
+                    case "÷":
+                        if (secondNumber != 0)
+                            _result /= secondNumber;
+                        break;
+                }
+                txtDisplay.Text = _result.ToString();
+                _input = _result.ToString();
+                _operator = string.Empty;
+            }
+        }
+
+        protected void MemoryClear(object sender, EventArgs e)
+        {
+            _memory = 0;
+        }
+
+        protected void MemoryRecall(object sender, EventArgs e)
+        {
+            txtDisplay.Text = _memory.ToString();
+            _input = _memory.ToString();
+        }
+
+        protected void MemoryPlus(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtDisplay.Text, out double value))
+            {
+                _memory += value;
+            }
+        }
+
+        protected void MemoryMinus(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtDisplay.Text, out double value))
+            {
+                _memory -= value;
+            }
+        }
+    }
+}
